5fd39a316fdfa7f00df69a5e28384c6854724aca
DayagRussellJaydenM <b2029rjmdayag@pshs.edu.ph>
2026-03-31 21:37:05 +0800

    43rd Commit

--- 4QPortfolioDayag/public/3rdGraded/script.js
+++ 4QPortfolioDayag/public/3rdGraded/script.js
@@ -1,5 +1,7 @@
 function plotPoint(x0, y0, x, y) {
     console.log(x0, y0, x, y) // checks the arguments passed to this function
+    var in1 = document.getElementById("in1");
+    var out1 = document.getElementById("out1");
     in1.innerHTML = x0 + " " + y0 + " " + x + " " + y;
     var point = document.createElement('div');  
     point.className = 'point';
@@ -8,19 +10,20 @@ function plotPoint(x0, y0, x, y) {
     point.style.bottom = (y - y0 + 200 - 5) + 'px'; // subtract half the height of the point to center it by changing bottom css property
     document.getElementById('coordinatePlane').appendChild(point);
 
-    if (x > x0 && y > y0){
-      out1.innerHTML = "NE"
+    if (x === x0 && y === y0){
+      out1.innerHTML = "divisa";
+    }
+    else if (x > x0 && y > y0){
+      out1.innerHTML = "NE";
     }
     else if (x < x0 && y < y0){
-      out1.innerHTML = "SO"
+      out1.innerHTML = "SO";
     }
     else if (x > x0 && y < y0){
-      out1.innerHTML = "SE"
+      out1.innerHTML = "SE";
     }
     else if (x < x0 && y > y0){
-      out1.innerHTML = "NO"
+      out1.innerHTML = "NO";
     }
-    else{
-      out1.innerHTML = "divisa"
-    }
-  }
+    
+}
